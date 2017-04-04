78e59feaa8477b42a4cb70cdc0b579b1464679f7
ashwinlagji <ashwin_lagji@persistent.co.in>
2017-04-04 17:51:31 +0530

    Initial Commit ..

---
+++ src/index.spec.js
@@ -0,0 +1,6 @@
+describe('Dummy Test', function() {
+    it('has a dummy spec to test 2 + 2', function() {
+        // An intentionally failing test. No code within expect() will never equal 4.
+        expect().toEqual(4);
+    });
+});
